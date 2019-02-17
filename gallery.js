0eb6e78727491cf1b85557242d9989f5149fae6f
lexinal <46660807+lexinal@users.noreply.github.com>
2019-02-17 15:40:59 -0500

    Added psuedocode and document ready function

---
+++ gallery.js
@@ -0,0 +1,61 @@
+// document ready function
+$(document).ready(() => {
+	// DEFINE YOUR VARIABLES
+	
+	//  select all the slides means the $ sign (value of the variable will be an array)
+	
+
+	// set the index slide (number) a.k.a where we start
+
+	// set the current slide based on the index
+
+	// STEPS
+	// 1. hide all slides except for the one we want to see (e.g. all but the first)
+		// a.
+		// b.
+	// 2. move in the specified direction
+		// a.
+		// b.
+		// c. return to the opposite or resetting
+
+	// INSTRUCTIONS
+	// hide all the slides
+
+	// show the first slide
+
+	// when the user clicks "next"
+		
+		// hide the current slide
+
+		// increment the slide index
+		slideIndex++; 
+			//same as slideIndex = slideIndex + 1
+			slideIndex + 1
+
+			// conditional check of the slideIndex (if greater than 4...)
+			
+				// reset to 0
+
+			// set the currentSlide based on the newly incremented slideIndex
+		
+				// show the next
+
+			// when the user clicks "previous"
+
+				// hide the current slide
+
+				// de-increment the slide index
+		slideIndex--; 
+					//same as slideIndex = slideIndex + 1
+			slideIndex - 1
+
+			// conditional check of the slideIndex (if less than 0...)
+			
+				// reset to 4
+
+			// set the currentSlide based on the newly incremented slideIndex
+		
+				// show the next
+
+//close the document ready function
+});
