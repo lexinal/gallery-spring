2fc3db0e9a73f70d4de6580d1765fcaaa714207b
lexinal <46660807+lexinal@users.noreply.github.com>
2019-02-17 18:16:49 -0500

    Change To Next Image

--- gallery.js
+++ gallery.js
@@ -13,4 +13,12 @@ $(document).ready(() => {
     $('#im_' + currentImage).stop().fadeIn(1);
   }); 
 
+	// Change to Next Image When User Clicks
+  	$('#next').on('click', function(){
+
+  	$('#im' + currentImage).stop().fadeOut(1);
+  	increaseImage();
+  	$('#im' + currentImage).stop().fadeIn(1);
+  });
+
   	
