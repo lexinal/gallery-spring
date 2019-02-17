53c0e792b67b4588bb2860578b386ca8638ebbdb
lexinal <46660807+lexinal@users.noreply.github.com>
2019-02-17 18:15:54 -0500

    Change Previous Image

--- gallery.js
+++ gallery.js
@@ -3,65 +3,14 @@ $(document).ready(() => {
 	//define your variables 
 	var currentImage = 1
 	var totalImages = 3;
-	// Previous Image 
-
-
-
 	
-	//  select all the slides means the $ sign (value of the variable will be an array)
-	
+	// Change for Previous Image When User Clicks
+	$('#previous').on('click', function(){
 
-	// set the index slide (number) a.k.a where we start
+	$('#im_' + currentImage).stop().fadeOut(1);
+    decreaseImage();
 
-	// set the current slide based on the index
+    $('#im_' + currentImage).stop().fadeIn(1);
+  }); 
 
-	// STEPS
-	// 1. hide all slides except for the one we want to see (e.g. all but the first)
-		// a.
-		// b.
-	// 2. move in the specified direction
-		// a.
-		// b.
-		// c. return to the opposite or resetting
-
-	// INSTRUCTIONS
-	// hide all the slides
-
-	// show the first slide
-
-	// when the user clicks "next"
-		
-		// hide the current slide
-
-		// increment the slide index
-		slideIndex++; 
-			//same as slideIndex = slideIndex + 1
-			slideIndex + 1
-
-			// conditional check of the slideIndex (if greater than 4...)
-			
-				// reset to 0
-
-			// set the currentSlide based on the newly incremented slideIndex
-		
-				// show the next
-
-			// when the user clicks "previous"
-
-				// hide the current slide
-
-				// de-increment the slide index
-		slideIndex--; 
-					//same as slideIndex = slideIndex + 1
-			slideIndex - 1
-
-			// conditional check of the slideIndex (if less than 0...)
-			
-				// reset to 4
-
-			// set the currentSlide based on the newly incremented slideIndex
-		
-				// show the next
-
-//close the document ready function
-});
+  	
